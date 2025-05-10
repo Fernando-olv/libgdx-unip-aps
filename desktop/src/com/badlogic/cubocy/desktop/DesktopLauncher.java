d3ebb3bad5b3cfb0e607e0760ddf67b8518cf36d
Fernando-olv <fernandoghw@gmail.com>
2025-05-09 23:59:03 -0300

    Alter sprites and change the name of the project.

--- desktop/src/com/badlogic/cubocy/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/cubocy/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Cubocy");
+        config.setTitle("MeuPet");
         config.setWindowedMode(800, 480);
         new Lwjgl3Application(new Cubocy(), config);
     }
